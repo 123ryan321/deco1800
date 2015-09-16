1d73e18f833b40618a86a6e32dbeeb9e2a2e7b6e
123Ryan321 <ryan.3@live.com.au>
2015-09-16 20:13:53 +1000

    made a game :)

--- js/game.js
+++ js/game.js
@@ -1,8 +1,11 @@
 // Create the canvas
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
-canvas.width = 512;
-canvas.height = 480;
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+canvas.width = 1124;
+canvas.height = 929;
+
 document.body.appendChild(canvas);
 
 // Background image
@@ -10,31 +13,110 @@ var bgReady = false;
 var bgImage = new Image();
 bgImage.onload = function () {
 	bgReady = true;
+	
 };
-bgImage.src = "images/background.png";
+bgImage.src = "images/background.jpg";
 
-// Hero image
-var heroReady = false;
-var heroImage = new Image();
-heroImage.onload = function () {
-	heroReady = true;
-};
-heroImage.src = "images/hero.png";
 
-// Monster image
-var monsterReady = false;
-var monsterImage = new Image();
-monsterImage.onload = function () {
-	monsterReady = true;
+
+// player image
+var playerReady = false;
+var playerImage = new Image();
+playerImage.onload = function () {
+	playerReady = true;
 };
-monsterImage.src = "images/monster.png";
+playerImage.src = "images/player.png";
+
+// node images
+var nodesReady_lck = false;
+var nodesReady_unlck = false;
+
+var nodeImage_lck = new Image();
+var nodeImage_unlck = new Image();
+nodeImage_lck.onload = function () {
+	nodesReady_lck = true;
+};
+nodeImage_unlck.onload = function () {
+	nodesReady_unlck = true;
+};
+nodeImage_lck.src = "images/node_lck.jpg";
+nodeImage_unlck.src = "images/node_unlck.jpg";
 
 // Game objects
-var hero = {
-	speed: 256 // movement in pixels per second
+var player = {
+	speed: 500, // movement in pixels per second
+	x: 0,
+	y: 0,
+
+	move: function(modifier){
+		//move player if keypad input
+		if (38 in keysDown) { // Player holding up
+			this.y -= this.speed * modifier;
+		}
+		if (40 in keysDown) { // Player holding down
+			this.y += this.speed * modifier;
+		}
+		if (37 in keysDown) { // Player holding left
+			this.x -= this.speed * modifier;
+		}
+		if (39 in keysDown) { // Player holding right
+			this.x += this.speed * modifier;
+		}
+
+	}
 };
-var monster = {};
-var monstersCaught = 0;
+
+var numUnlcked = 0;	//number of unlocked nodes
+
+function Node (name, x, y) {
+	this.name = name;
+	this.x = x; 
+	this.y = y;
+
+	this.locked = true;
+	this.unlock = function() {
+		this.locked = false;
+		//change the image too
+
+		//update number of unlocked
+		++numUnlcked;
+	}
+};
+
+var nodes = [
+	new Node("QLD_sth", 975, 350),
+	new Node("QLD_nth", 860, 150),
+	new Node("NSW", 900, 550),
+	new Node("ACT", 970, 610),
+	new Node("VIC", 800, 720),
+	new Node("TAS", 850, 860),
+	new Node("SA", 510, 500),
+	new Node("WA", 150, 500),
+	new Node("NT", 560, 250),
+	];
+
+var score = {
+	curr: 0,
+	mult: 0,	//multiplier
+	add: 10,	//adding score
+	sub: -5,	//subtract for question wrong
+
+	increase: function() {
+		//Increase the score if a node has been passed
+		++this.mult;
+		this.curr += this.add*this.mult;
+	},
+
+	decrease: function() {
+		//decrease score if a node was wrongly answered
+		this.mult = 0;
+
+		this.curr += this.sub;
+		if(this.curr < 0) {
+			this.curr = 0;
+		}
+	}
+};
 
 // Handle keyboard controls
 var keysDown = {};
@@ -47,40 +129,72 @@ addEventListener("keyup", function (e) {
 	delete keysDown[e.keyCode];
 }, false);
 
-// Reset the game when the player catches a monster
-var reset = function () {
-	hero.x = canvas.width / 2;
-	hero.y = canvas.height / 2;
+// init the game when the player catches a node
+var init = function () {
+	//Start Player at a Node / random place on a path
+	player.x = canvas.width / 2;
+	player.y = canvas.height / 2;
 
-	// Throw the monster somewhere on the screen randomly
-	monster.x = 32 + (Math.random() * (canvas.width - 64));
-	monster.y = 32 + (Math.random() * (canvas.height - 64));
+	// Throw the node somewhere on the screen randomly
+	// nodes[0].x = 32 + (Math.random() * (canvas.width - 64));
+	// nodes[0].y = 32 + (Math.random() * (canvas.height - 64));
 };
+
+
+
+//returns if the player has reached a node
+var isAtNode = function() {
+	w = (nodeImage_lck.width  + playerImage.width) / 2;
+	h = (nodeImage_lck.height + playerImage.height) / 2;
+
+
+	for (i = 0; i < nodes.length; i++) {
+		if (player.x <= (nodes[i].x + w) && nodes[i].x <= (player.x + w)
+			&& player.y <= (nodes[i].y + h) && nodes[i].y <= (player.y + h)) 	{
+			
+			return {
+				bool: true,
+				idx: i,
+			}
+		}
+	}
+
+	return false;
+}
 
 // Update game objects
 var update = function (modifier) {
-	if (38 in keysDown) { // Player holding up
-		hero.y -= hero.speed * modifier;
-	}
-	if (40 in keysDown) { // Player holding down
-		hero.y += hero.speed * modifier;
-	}
-	if (37 in keysDown) { // Player holding left
-		hero.x -= hero.speed * modifier;
-	}
-	if (39 in keysDown) { // Player holding right
-		hero.x += hero.speed * modifier;
+
+	//move from keypad input
+	player.move(modifier);
+
+	//check if we have reached a node
+	check = isAtNode();
+
+	if(check) {
+
+
+		alert("You Now have to Answer a Question about \n\n" + nodes[check.idx].name);
+		popup("popUpDiv");
+		//if correct
+		if(nodes[check.idx].locked) {
+			//locked so unlock it
+			nodes[check.idx].unlock();
+		}
+		score.increase();
+
+		// //if incorrect
+		// score.decrease();
+		// //move off node 
+
+		init();
 	}
 
-	// Are they touching?
-	if (
-		hero.x <= (monster.x + 32)
-		&& monster.x <= (hero.x + 32)
-		&& hero.y <= (monster.y + 32)
-		&& monster.y <= (hero.y + 32)
-	) {
-		++monstersCaught;
-		reset();
+
+	//check if game over
+	if (numUnlcked == nodes.length) {
+		//game over
+		alert("GAME OVER - COMPLETED");
 	}
 };
 
@@ -90,12 +204,22 @@ var render = function () {
 		ctx.drawImage(bgImage, 0, 0);
 	}
 
-	if (heroReady) {
-		ctx.drawImage(heroImage, hero.x, hero.y);
+	if (playerReady) {
+		ctx.drawImage(playerImage, player.x, player.y);
 	}
 
-	if (monsterReady) {
-		ctx.drawImage(monsterImage, monster.x, monster.y);
+
+	if (nodesReady_lck && nodesReady_unlck) {
+		for(i = 0; i < nodes.length ; i ++) {
+			if (nodes[i].locked) {
+				//draw locked image
+				ctx.drawImage(nodeImage_lck, nodes[i].x, nodes[i].y);
+			} else {
+				//draw unlocked image
+				ctx.drawImage(nodeImage_unlck, nodes[i].x, nodes[i].y);
+			}		
+		}
+		
 	}
 
 	// Score
@@ -103,7 +227,7 @@ var render = function () {
 	ctx.font = "24px Helvetica";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
-	ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+	ctx.fillText("Score: " + score.curr, 32, 32);
 };
 
 // The main game loop
@@ -118,6 +242,10 @@ var main = function () {
 
 	// Request to do this again ASAP
 	requestAnimationFrame(main);
+
+
+
+	
 };
 
 // Cross-browser support for requestAnimationFrame
@@ -126,5 +254,5 @@ requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame
 
 // Let's play this game!
 var then = Date.now();
-reset();
+init();
 main();
